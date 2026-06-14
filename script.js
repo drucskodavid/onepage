6f40180a67f54a40d0bbca64561eb0467cc48e25
drucskodavid <147561080+drucskodavid@users.noreply.github.com>
2026-06-14 14:28:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const params = new URLSearchParams(window.location.search);
+
+const data = {
+    city: params.get("city") || "Szombathely",
+    state: params.get("state") || "Vas",
+    name: params.get("name") || "Minta Tamás Norbert",
+    phone: params.get("phone") || "+36 30 123 4567",
+    mail: params.get("mail") || "info@minta.hu",
+    address: params.get("address") || "Szombathely, Minta utca 12/A",
+    market: params.get("market") || "Villanyszerelés"
+};
+
+document.querySelectorAll("[data-var]").forEach(el => {
+    const key = el.dataset.var;
+
+    if (data[key] !== undefined) {
+        el.textContent = data[key];
+    }
+});
+
+
+
+
 /* Smooth scrolling */
 $(document).ready(function(){
     $("a").on('click', function(event) {
@@ -31,3 +54,24 @@ scrollToTopBtn.onclick = function(){
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 */
+
+let demoNotice = document.getElementById('demo');
+if (localStorage.getItem("democlosed") == 'y') {
+  closeNotice();
+}
+
+if (params.get("skipdemo") == 0) {
+  localStorage.clear();
+  location.href = '/';
+}
+
+if (params.get("skipdemo") == 1) {
+  closeNotice();
+  location.href = '/';
+}
+
+
+function closeNotice() {
+  demoNotice.style.display = 'none';
+  localStorage.setItem("democlosed", "y");
+}
